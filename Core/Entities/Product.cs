9832e4ae02883b5910272082669b3b50ffe057cd
medalida <medalida47@gmail.com>
2024-10-27 20:27:48 +0100

    feat: add Product classes

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public required string Name { get; set; }
+
+    public required string Description { get; set; }
+    
+    public decimal Price { get; set; }
+    
+    public required string PictureUrl { get; set; }
+    
+    public required string Type { get; set; } = string.Empty;
+    
+    public required string Brand { get; set; } = string.Empty;
+    
+    public int QuantityInStock { get; set; }
+}
