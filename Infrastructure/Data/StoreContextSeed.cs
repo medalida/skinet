ad14fdff4666c5e86ecd8423389464604fc3e2a1
medalida <medalida47@gmail.com>
2024-10-28 21:45:05 +0100

    feat: seed data (products.json)

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data;
+
+public class StoreContextSeed
+{
+    public static async Task SeedAsync(StoreContext context)
+    {
+        if (!context.Products.Any())
+        {
+            var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+            if (products == null) return;
+
+            await context.Products.AddRangeAsync(products);
+            await context.SaveChangesAsync();
+        }
+    }
+}
