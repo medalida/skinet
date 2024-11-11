61e655bd6223bf66deea6f4dfe73699b545edcce
medalida <medalida47@gmail.com>
2024-11-11 19:32:47 +0100

    feat: product details page

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -3,11 +3,18 @@ import { ShopService } from '../../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { _isNumberValue } from '@angular/cdk/coercion';
 import { Product } from '../../../shared/modules/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatDivider } from '@angular/material/divider';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatInputModule, MatDivider, MatLabel],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
